29519e912d03e29be0d570de94e2fca8f2b6be78
Mokha Lerthsuwanroj <70966063+namolert@users.noreply.github.com>
2025-02-15 23:14:02 +0000

    feat: logout

--- unihand/unihand/settings.py
+++ unihand/unihand/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+LOGIN_URL = "login"
--- unihand/users/urls.py
+++ unihand/users/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]
