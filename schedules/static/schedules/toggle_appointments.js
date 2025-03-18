20c764f3c250d80853a9a9f87a1409749145722b
Mokha Lerthsuwanroj <70966063+namolert@users.noreply.github.com>
2025-03-18 22:25:31 +0000

    Change toggle-appointment logic

--- schedules/static/schedules/toggle_appointments.js
+++ schedules/static/schedules/toggle_appointments.js
@@ -1,16 +1,8 @@
 function toggleAppointments() {
-  // Get all schedule rows
   const checkBox = document.getElementById("showAppointments");
+  const appointmentRows = document.querySelectorAll(".appointments");
 
-  // Iterate through all course schedules and toggle the appointments display
-  const scheduleIds = document.querySelectorAll(".schedule-row");
-  scheduleIds.forEach((row) => {
-    const appointmentsDiv = row.querySelector(".appointments");
-    // Toggle display based on checkbox state
-    if (checkBox.checked) {
-      appointmentsDiv.style.display = "block";
-    } else {
-      appointmentsDiv.style.display = "none";
-    }
+  appointmentRows.forEach((row) => {
+    row.style.display = checkBox.checked ? "table-row" : "none";
   });
 }
