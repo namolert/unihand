91ecc5e35a2e76a9f714d7b5626c52515d945be5
gaurav-S8 <3043108s@student.gla.ac.uk>
2025-03-19 15:32:55 +0000

    Frontend Code added

--- schedules/views.py
+++ schedules/views.py
@@ -8,8 +8,12 @@ def student_course_schedule(request):
     student = request.user.student  
     course_schedule = CourseSchedule.objects.filter(course__enrollments__student=student)
 
+    # List of days of the week
+    days_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
     context = {
         "course_schedule": course_schedule,
+        "days_of_week": days_of_week,
     }
     return render(request, "schedules/students/course_schedule.html", context)
 
--- todos/static/todos/todos.js
+++ todos/static/todos/todos.js
@@ -21,3 +21,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// Handle add task confirmation
+document.addEventListener('DOMContentLoaded', function() {
+  const modal = document.getElementById("addTaskModal");
+  const btn = document.getElementById("addTaskBtn_Outside");
+  const span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+    console.log("Something");
+  }
+
+  // Close modal when the close button (X) is clicked
+  span.onclick = function() {
+    modal.style.display = "none"; 
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+});
